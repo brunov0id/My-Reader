974d3581b1388ba6683b7a548587fda96e7b276f
Bruno da Luz <brunolabx@gmail.com>
2015-06-25 10:21:39 -0300

    Logout

--- app/src/main/java/betweenbits/myreader/ControleGeralActivity.java
+++ app/src/main/java/betweenbits/myreader/ControleGeralActivity.java
@@ -1,5 +1,7 @@
 package betweenbits.myreader;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +14,8 @@ public class ControleGeralActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controle_geral);
+
+        setTitle("My Reader");
     }
 
     @Override
@@ -30,5 +34,26 @@ public class ControleGeralActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Logout ?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
