0fb34afe31d481c071b50ae1ec45eeb5d0a89eb4
brn0s <brunolabx@gmail.com>
2015-06-25 23:02:29 -0300

    Add Icons and Drawer

--- app/src/main/java/betweenbits/myreader/ControleGeralActivity.java
+++ app/src/main/java/betweenbits/myreader/ControleGeralActivity.java
@@ -4,18 +4,58 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class ControleGeralActivity extends ActionBarActivity {
+
+    private Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controle_geral);
 
-        setTitle("My Reader");
+        myToolbar = (Toolbar) findViewById(R.id.myToolbar);
+        myToolbar.setLogo(R.drawable.rss);
+        myToolbar.setTitle("My Reader");
+        setSupportActionBar(myToolbar);
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(myToolbar)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Add Feed").withIcon(R.drawable.add),
+                        new SecondaryDrawerItem().withName("Favorite Entries").withIcon(R.drawable.folder_special),
+                        new DividerDrawerItem()
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+
+                        return false;
+                    }
+                })
+                .withActionBarDrawerToggleAnimated(true)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(0)
+                .withDisplayBelowToolbar(true)
+                .build();
+        result.setSelectionByIdentifier(1);
+        result.openDrawer();
+        result.closeDrawer();
+        result.isDrawerOpen();
     }
 
     @Override
